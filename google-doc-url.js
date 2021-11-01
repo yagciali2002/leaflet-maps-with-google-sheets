7b623e50531aaffcd945b0ee35c472690b63d7f7
Ali Levent YAGCI <47858837+yagciali2002@users.noreply.github.com>
2021-11-01 19:10:45 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/13JMpXxaIyoM1O21jrn7z0irc1N3xSSh5VwGkFhMRJr0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyC_yJri_0BW2aaQCL58zOJXHeoWGFEVQ-0';
